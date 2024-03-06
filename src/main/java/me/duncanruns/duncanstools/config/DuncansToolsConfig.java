67aecdda23102d5c752821e945118aa6a18d5940
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-03-06 14:38:09 -0400

    Fix config

--- src/main/java/me/duncanruns/duncanstools/config/DuncansToolsConfig.java
+++ src/main/java/me/duncanruns/duncanstools/config/DuncansToolsConfig.java
@@ -7,21 +7,27 @@ import dev.isxander.yacl3.api.controller.BooleanControllerBuilder;
 import dev.isxander.yacl3.api.controller.DoubleSliderControllerBuilder;
 import dev.isxander.yacl3.api.controller.IntegerFieldControllerBuilder;
 import dev.isxander.yacl3.api.controller.StringControllerBuilder;
-import dev.isxander.yacl3.config.ConfigEntry;
-import dev.isxander.yacl3.config.GsonConfigInstance;
+import dev.isxander.yacl3.config.v2.api.ConfigClassHandler;
 import dev.isxander.yacl3.config.v2.api.SerialEntry;
+import dev.isxander.yacl3.config.v2.api.serializer.GsonConfigSerializerBuilder;
 import me.duncanruns.duncanstools.DuncansTools;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 import java.nio.file.Path;
 
 public class DuncansToolsConfig implements ModMenuApi {
 
     private static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve(DuncansTools.MOD_ID + ".json");
-    private static final GsonConfigInstance<DuncansToolsConfig> CONFIG_INSTANCE = GsonConfigInstance.createBuilder(DuncansToolsConfig.class).setPath(CONFIG_PATH).build();
+    //    private static final GsonConfigInstance<DuncansToolsConfig> CONFIG_INSTANCE = GsonConfigInstance.createBuilder(DuncansToolsConfig.class).setPath(CONFIG_PATH).build();
+    private static final ConfigClassHandler<DuncansToolsConfig> CONFIG_INSTANCE = ConfigClassHandler.createBuilder(DuncansToolsConfig.class)
+            .id(Identifier.of(DuncansTools.MOD_ID, "config"))
+            .serializer(config -> GsonConfigSerializerBuilder.create(config)
+                    .setPath(CONFIG_PATH)
+                    .build()).build();
 
     @SerialEntry
     public boolean alignmentLockerEnabled = false;
@@ -72,7 +78,7 @@ public class DuncansToolsConfig implements ModMenuApi {
                                         .description(OptionDescription.createBuilder()
                                                 .text(Text.of("Enables the alignment locker module, which locks the player's yaw rotation to a set of angles."))
                                                 .build())
-                                        .binding(CONFIG_INSTANCE.getDefaults().alignmentLockerEnabled, () -> getInstance().alignmentLockerEnabled, val -> getInstance().alignmentLockerEnabled = val)
+                                        .binding(CONFIG_INSTANCE.defaults().alignmentLockerEnabled, () -> getInstance().alignmentLockerEnabled, val -> getInstance().alignmentLockerEnabled = val)
                                         .controller(opt -> BooleanControllerBuilder.create(opt).onOffFormatter())
                                         .build())
                                 .option(Option.<Boolean>createBuilder()
@@ -80,7 +86,7 @@ public class DuncansToolsConfig implements ModMenuApi {
                                         .description(OptionDescription.createBuilder()
                                                 .text(Text.of("Enables the bedrock lever module, which allows the player to spam a lever 64 times per tick, which lags the server-side enough to allow bedrock breaking."))
                                                 .build())
-                                        .binding(CONFIG_INSTANCE.getDefaults().bedrockLeverEnabled, () -> getInstance().bedrockLeverEnabled, val -> getInstance().bedrockLeverEnabled = val)
+                                        .binding(CONFIG_INSTANCE.defaults().bedrockLeverEnabled, () -> getInstance().bedrockLeverEnabled, val -> getInstance().bedrockLeverEnabled = val)
                                         .controller(opt -> BooleanControllerBuilder.create(opt).onOffFormatter())
                                         .build())
                                 .option(Option.<Boolean>createBuilder()
@@ -88,7 +94,7 @@ public class DuncansToolsConfig implements ModMenuApi {
                                         .description(OptionDescription.createBuilder()
                                                 .text(Text.of("Enables the trade finder module, which allows the usage of /findbooktrade which re-opens a new librarian's trade menu until the correct trades are found. Requires a mod which re-rolls the trades every time the menu of a new librarian is opened (such as Duncan's Tweaks)."))
                                                 .build())
-                                        .binding(CONFIG_INSTANCE.getDefaults().bookTradeFinderEnabled, () -> getInstance().bookTradeFinderEnabled, val -> getInstance().bookTradeFinderEnabled = val)
+                                        .binding(CONFIG_INSTANCE.defaults().bookTradeFinderEnabled, () -> getInstance().bookTradeFinderEnabled, val -> getInstance().bookTradeFinderEnabled = val)
                                         .controller(opt -> BooleanControllerBuilder.create(opt).onOffFormatter())
                                         .build())
                                 .option(Option.<Boolean>createBuilder()
@@ -96,7 +102,7 @@ public class DuncansToolsConfig implements ModMenuApi {
                                         .description(OptionDescription.createBuilder()
                                                 .text(Text.of("Enables the craft refill module, which allows the player to refill a crafting table or stonecutter with the last selected recipe by pressing shift+space, similar to villager refilling. Also allows the usage of ctrl+shift+space to spam craft (works best with carpet's ctrlQCraftingFix)."))
                                                 .build())
-                                        .binding(CONFIG_INSTANCE.getDefaults().craftRefillEnabled, () -> getInstance().craftRefillEnabled, val -> getInstance().craftRefillEnabled = val)
+                                        .binding(CONFIG_INSTANCE.defaults().craftRefillEnabled, () -> getInstance().craftRefillEnabled, val -> getInstance().craftRefillEnabled = val)
                                         .controller(opt -> BooleanControllerBuilder.create(opt).onOffFormatter())
                                         .build())
                                 .option(Option.<Boolean>createBuilder()
@@ -104,7 +110,7 @@ public class DuncansToolsConfig implements ModMenuApi {
                                         .description(OptionDescription.createBuilder()
                                                 .text(Text.of("Enables the alignment locker module, which locks the player's yaw rotation to a set of angles."))
                                                 .build())
-                                        .binding(CONFIG_INSTANCE.getDefaults().farmClickerEnabled, () -> getInstance().farmClickerEnabled, val -> getInstance().farmClickerEnabled = val)
+                                        .binding(CONFIG_INSTANCE.defaults().farmClickerEnabled, () -> getInstance().farmClickerEnabled, val -> getInstance().farmClickerEnabled = val)
                                         .controller(opt -> BooleanControllerBuilder.create(opt).onOffFormatter())
                                         .build())
                                 .option(Option.<Boolean>createBuilder()
@@ -112,7 +118,7 @@ public class DuncansToolsConfig implements ModMenuApi {
                                         .description(OptionDescription.createBuilder()
                                                 .text(Text.of("Enables the gamma override module, which overrides the gamma level of vanilla's \"bright\" setting."))
                                                 .build())
-                                        .binding(CONFIG_INSTANCE.getDefaults().gammaOverrideEnabled, () -> getInstance().gammaOverrideEnabled, val -> getInstance().gammaOverrideEnabled = val)
+                                        .binding(CONFIG_INSTANCE.defaults().gammaOverrideEnabled, () -> getInstance().gammaOverrideEnabled, val -> getInstance().gammaOverrideEnabled = val)
                                         .controller(opt -> BooleanControllerBuilder.create(opt).onOffFormatter())
                                         .build())
                                 .option(Option.<Boolean>createBuilder()
@@ -120,7 +126,7 @@ public class DuncansToolsConfig implements ModMenuApi {
                                         .description(OptionDescription.createBuilder()
                                                 .text(Text.of("Enables the librarian book helper module, which overlays book enchantments in the villager trading GUI without having to hover over the book. It also lets you set a highlighted enchantment ID which causes a ding sound to be played when the correct enchantment appears, and will highlight the enchantment in bold green."))
                                                 .build())
-                                        .binding(CONFIG_INSTANCE.getDefaults().librarianBookHelperEnabled, () -> getInstance().librarianBookHelperEnabled, val -> getInstance().librarianBookHelperEnabled = val)
+                                        .binding(CONFIG_INSTANCE.defaults().librarianBookHelperEnabled, () -> getInstance().librarianBookHelperEnabled, val -> getInstance().librarianBookHelperEnabled = val)
                                         .controller(opt -> BooleanControllerBuilder.create(opt).onOffFormatter())
                                         .build())
                                 .option(Option.<Boolean>createBuilder()
@@ -128,7 +134,7 @@ public class DuncansToolsConfig implements ModMenuApi {
                                         .description(OptionDescription.createBuilder()
                                                 .text(Text.of("Enables the portal coords module, which allows the usage of the /portal command and the relevant keybind which converts the player's coordinates to the other dimension's coordinates."))
                                                 .build())
-                                        .binding(CONFIG_INSTANCE.getDefaults().portalCoordsEnabled, () -> getInstance().portalCoordsEnabled, val -> getInstance().portalCoordsEnabled = val)
+                                        .binding(CONFIG_INSTANCE.defaults().portalCoordsEnabled, () -> getInstance().portalCoordsEnabled, val -> getInstance().portalCoordsEnabled = val)
                                         .controller(opt -> BooleanControllerBuilder.create(opt).onOffFormatter())
                                         .build())
                                 .build())
@@ -139,7 +145,7 @@ public class DuncansToolsConfig implements ModMenuApi {
                                         .description(OptionDescription.createBuilder()
                                                 .text(Text.of("Sets the interval between clicks, measured in ticks. 20 ticks = 1 second."))
                                                 .build())
-                                        .binding(CONFIG_INSTANCE.getDefaults().clickerInterval, () -> getInstance().clickerInterval, val -> getInstance().clickerInterval = val)
+                                        .binding(CONFIG_INSTANCE.defaults().clickerInterval, () -> getInstance().clickerInterval, val -> getInstance().clickerInterval = val)
                                         .controller(integerOption -> IntegerFieldControllerBuilder.create(integerOption).min(1))
                                         .build())
                                 .option(Option.<Boolean>createBuilder()
@@ -147,7 +153,7 @@ public class DuncansToolsConfig implements ModMenuApi {
                                         .description(OptionDescription.createBuilder()
                                                 .text(Text.of("Uses the item in hand or interacts with the targeted block as if you were right clicking instead of attacking/mining."))
                                                 .build())
-                                        .binding(CONFIG_INSTANCE.getDefaults().clickerDoUse, () -> getInstance().clickerDoUse, val -> getInstance().clickerDoUse = val)
+                                        .binding(CONFIG_INSTANCE.defaults().clickerDoUse, () -> getInstance().clickerDoUse, val -> getInstance().clickerDoUse = val)
                                         .controller(opt -> BooleanControllerBuilder.create(opt).yesNoFormatter())
                                         .build())
                                 .build())
@@ -158,7 +164,7 @@ public class DuncansToolsConfig implements ModMenuApi {
                                         .description(OptionDescription.createBuilder()
                                                 .text(Text.of("Specifies the actual gamma wanted when the in-game gamma is set to \"Bright\"."))
                                                 .build())
-                                        .binding(CONFIG_INSTANCE.getDefaults().brightGamma, () -> getInstance().brightGamma, val -> getInstance().brightGamma = val)
+                                        .binding(CONFIG_INSTANCE.defaults().brightGamma, () -> getInstance().brightGamma, val -> getInstance().brightGamma = val)
                                         .controller(opt -> DoubleSliderControllerBuilder.create(opt).range(1d, 20d).step(0.1d))
                                         .build())
                                 .build())
@@ -174,7 +180,7 @@ public class DuncansToolsConfig implements ModMenuApi {
                                                         "fire_protection 3\n" +
                                                         "minecraft:frost_walker 2"))
                                                 .build())
-                                        .binding(CONFIG_INSTANCE.getDefaults().librarianHighlight, () -> CONFIG_INSTANCE.getConfig().librarianHighlight, val -> getInstance().librarianHighlight = val)
+                                        .binding(CONFIG_INSTANCE.defaults().librarianHighlight, () -> CONFIG_INSTANCE.instance().librarianHighlight, val -> getInstance().librarianHighlight = val)
                                         .controller(StringControllerBuilder::create)
                                         .build())
                                 .option(Option.<Boolean>createBuilder()
@@ -182,7 +188,7 @@ public class DuncansToolsConfig implements ModMenuApi {
                                         .description(OptionDescription.createBuilder()
                                                 .text(Text.of("Will make a \"ding\" sound when opening the villager trading GUI if the offers include a book with the highlighted enchantment."))
                                                 .build())
-                                        .binding(CONFIG_INSTANCE.getDefaults().librarianHighlightDing, () -> CONFIG_INSTANCE.getConfig().librarianHighlightDing, val -> getInstance().librarianHighlightDing = val)
+                                        .binding(CONFIG_INSTANCE.defaults().librarianHighlightDing, () -> CONFIG_INSTANCE.instance().librarianHighlightDing, val -> getInstance().librarianHighlightDing = val)
                                         .controller(opt -> BooleanControllerBuilder.create(opt).onOffFormatter())
                                         .build())
                                 .build())
@@ -192,7 +198,7 @@ public class DuncansToolsConfig implements ModMenuApi {
     }
 
     public static DuncansToolsConfig getInstance() {
-        return CONFIG_INSTANCE.getConfig();
+        return CONFIG_INSTANCE.instance();
     }
 
     public String getLibrarianHighlight() {
